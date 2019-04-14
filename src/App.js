b8f93cdbd3c9926356c468eccb4eee87e0a6452f
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-14 16:04:49 -0500

    Create TodoList component.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,30 @@ function Todo({ todo, idx }) {
   return <div className="todo">{todo.text}</div>;
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add a new todo..."
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 // create our state within App component, using useState
 // this of 'setTodos' as 'this.state' and useState as 'this.state.setState()'
 // by default, the state of todos has three todos
@@ -24,12 +48,18 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, idx) => (
           <Todo key={idx} index={idx} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
