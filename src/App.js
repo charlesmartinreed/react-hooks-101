e252f6053ab57e3902720202532e131d0b390708
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-14 16:12:51 -0500

    Add ability to mark Todo as complete.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Todo({ todo, idx }) {
-  return <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo }) {
+  return (
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "none" }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
 }
 
 function TodoForm({ addTodo }) {
@@ -53,11 +63,22 @@ function App() {
     setTodos(newTodos);
   };
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
-        {todos.map((todo, idx) => (
-          <Todo key={idx} index={idx} todo={todo} />
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
