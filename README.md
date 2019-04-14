07357f3dacc346f785bca1eb600a1defa71770dd
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-14 15:52:57 -0500

    Create App.js. Create Todo component in App.js, pass state with Hooks.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+This project was adapted from:
+[Scotch.io](https://scotch.io/tutorials/build-a-react-to-do-app-with-react-hooks-no-class-components).
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function Todo({ todo, idx }) {
+  return <div className="todo">{todo.text}</div>;
+}
+
+// create our state within App component, using useState
+// this of 'setTodos' as 'this.state' and useState as 'this.state.setState()'
+// by default, the state of todos has three todos
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Go for a quick jog",
+      isCompleted: false
+    },
+    {
+      text: "Watch the new episode of  Game of Thrones",
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, idx) => (
+          <Todo key={idx} index={idx} todo={todo} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
